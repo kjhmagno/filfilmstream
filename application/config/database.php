e30d4c6d0b24fff914b04189062d741d355d5ee2
Kat Magno <katrinajoymagno@gmail.com>
2012-02-08 19:04:22 +0800

    branch test

--- application/config/database.php
+++ application/config/database.php
@@ -49,7 +49,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'icsuplb';
+$db['default']['password'] = 'july1388';
 $db['default']['database'] = 'filfilmstream';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
